5d9221c3770150bcf1239123203163112688b6da
Silvio Gregorini <silviogregorini@openforce.it>
2019-12-09 11:12:40 +0100

    [UPD][l10n_it_enasarco] Updated files copyright

--- l10n_it_enasarco/models/res_company.py
+++ l10n_it_enasarco/models/res_company.py
@@ -1,3 +1,5 @@
+# Copyright 2018 Alessandro Camilli <alessandrocamilli@openforce.it>
+# Copyright 2019 Silvio Gregorini <silviogregorini@openforce.it>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from odoo import _, fields, models
