97c908176b8668626223ad65564bdd5fbefc9b67
Docker @ kuntrawant <matteomircoli@openforce.it>
2022-07-06 16:58:36 +0200

    [FIX][l10n_it_fatturapa_in] https://github.com/OCA/l10n-italy/pull/2858

--- l10n_it_fatturapa_in/__manifest__.py
+++ l10n_it_fatturapa_in/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'ITA - Fattura elettronica - Ricezione',
-    'version': '12.0.2.9.4+PR1676',
+    'version': '12.0.2.9.4+PR1676+PR2858',
     "development_status": "Beta",
     'category': 'Localization/Italy',
     'summary': 'Ricezione fatture elettroniche',
