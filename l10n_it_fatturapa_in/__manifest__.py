9b9af548955b749516000533dc597c517a7f1176
suassa <susannaortini@openforce.it>
2022-11-18 16:48:54 +0100

    [ADD][l10n_it_fatturapa_id] Add PR https://github.com/OCA/l10n-italy/pull/1676

--- l10n_it_fatturapa_in/__manifest__.py
+++ l10n_it_fatturapa_in/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'ITA - Fattura elettronica - Ricezione',
-    'version': '12.0.3.1.3',
+    'version': '12.0.3.1.3+PR1676',
     "development_status": "Beta",
     'category': 'Localization/Italy',
     'summary': 'Ricezione fatture elettroniche',
